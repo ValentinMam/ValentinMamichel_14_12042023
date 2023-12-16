f4301aac55fab6506bb2a4ec01b8c6ee769ce305
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2023-12-16 16:00:20 +0100

    delete first React project

--- OC-P14-HRNet-React/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable comma-dangle */
-import { combineReducers } from 'redux'
-import EmployeeReducer from './employee.reducer.js'
-
-/** Création du reducer */
-export default combineReducers({
-  employees: EmployeeReducer,
-})
